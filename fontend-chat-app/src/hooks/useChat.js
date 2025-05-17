2397c3bc682d3edae6ad0519cc7b1dc421b23c5d
msncoder <msncoder1999@gmail.com>
2025-05-17 14:57:03 +0500

    useChat hook complete

---
+++ fontend-chat-app/src/hooks/useChat.js
@@ -0,0 +1,70 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+import useWebSocket from "react-use-websocket";
+
+export const useChat = (roomName, username) => {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const messagesEndRef = useRef(null);
+
+  const { sendMessage, lastMessage } = useWebSocket(
+    `ws://localhost:8000/ws/chat/${roomName}`,
+    {
+      shouldReconnect: () => true,
+      reconnectAttemps: 10,
+      reconnectInterval: 3000,
+    }
+  );
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/api/rooms/${roomName}/messages/`
+        );
+        const data = await response.json();
+        setMessages(data); // ✅ ye sahi tarika hai
+        setIsLoading(false);
+      } catch (err) {
+        setError("Failed to load message");
+        setIsLoading(false);
+      }
+    };
+    fetchMessages();
+  }, [roomName]);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const data = JSON.parse(lastMessage.data);
+      setMessage((prev = [...prev, data]));
+    }
+  }, [lastMessage]);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  const sendChatMessage = useCallback(() => {
+    if (message.trim()) {
+      sendMessage(
+        json.stringyfy({
+          message,
+          username,
+          room_name: roomName,
+        })
+      );
+      setMessage();
+    }
+  }, [message, username, roomName, sendMessage]);
+
+  return {
+    messages,
+    message,
+    setMessage,
+    isLoading,
+    error,
+    sendChatMessage,
+    messagesEndRef,
+  };
+};
